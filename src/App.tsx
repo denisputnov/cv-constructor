b41a0a9ab03653d836a82636a220cf7513cc8401
Денис Путнов <grnbows.connect@mail.ru>
2022-02-02 21:34:35 +0300

    add basic editor layout and improve image uploader functionality (drag and drop, cropping, preview added)

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,26 @@
-import React, { useCallback, useEffect, useRef, useState } from 'react';
-import Cropper from "react-easy-crop";
-import { Point, Area } from "react-easy-crop/types";
+import React from 'react';
 import styled from 'styled-components';
-import ImageUploader from './components/ImageUploader';
+import ImageUploader from './components/ImageUploader/ImageUploader';
+import "antd/dist/antd.css";
+import Editor from './components/Editor';
 
 export default function App() {
-  // const [crop, setCrop] = useState<Point>({ x: 0, y: 0 });
-  // const [zoom, setZoom] = useState(1);
-  // const onCropComplete = useCallback(
-  //   (croppedArea: Area, croppedAreaPixels: Area) => {
-  //     console.log(croppedArea, croppedAreaPixels);
-  //   },
-  //   []
-  // );
-  
   // return (
-  //   <div className="App">
-  //     <div className="crop-container">
-  //       <Cropper
-  //         image="https://img.huffingtonpost.com/asset/5ab4d4ac2000007d06eb2c56.jpeg?cache=sih0jwle4e&ops=1910_1000"
-  //         crop={crop}
-  //         zoom={zoom}
-  //         aspect={4 / 3}
-  //         onCropChange={setCrop}
-  //         onCropComplete={onCropComplete}
-  //         onZoomChange={setZoom}
-  //       />
-  //     </div>
-  //   </div>
-  // );
+  //   <ImageUploadContainer>
+  //     <ImageUploader />
+  //   </ImageUploadContainer>
+  // )
   return (
-    <ImageUploadContainer>
-      <ImageUploader />
-    </ImageUploadContainer>
+    <Editor></Editor>
   )
 }
 
-const ImageUploadContainer = styled.div`
-  width: 400px;
-  padding: 20px;
-`
+// const ImageUploadContainer = styled.div`
+//   /* width: 400px; */
+//   height: 80vh;
+//   padding: 20px;
+//   display: flex;
+//   & > div {
+//     width: 400px;
+//   }
+// `
