e222a649ef53594afdb48ae8327c0611c780c08d
Денис Путнов <grnbows.connect@mail.ru>
2022-02-20 22:13:34 +0300

    add unsupported screen resolution message

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import "antd/dist/antd.min.css";
 import Editor from './pages/Editor/Editor';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UnsupportedResolution from './pages/UnsupportedResolution';
+import useResolutionSupported from './hooks/useResolutionSupported';
 
 export default function App() {
+  const resolutionSupported: boolean = useResolutionSupported()
+  console.log(resolutionSupported)
+  if (!resolutionSupported) return <UnsupportedResolution />
   return (
     <BrowserRouter>
       <Routes>
